e6feedaa274b884f88ab523214b83331071de01b
pethum123 <pethum334desilva@gmail.com>
2017-10-06 22:38:59 +0530

    fff

---
+++ src/MySampleService/AppSettings.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySample
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+
+        public bool CacheDbResults { get; set; } = true;
+    }
+}
